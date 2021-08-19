bd44437b29370916d62af21ea7b600019d653358
8thDawn <agevanz52@gmail.com>
2021-08-19 23:31:12 +0000

    first commit

---
+++ Name.js
@@ -0,0 +1,4 @@
+const lastName = 'Adjei';
+const firstName = 'Evans';
+
+console.log(`My name is ${firstName} ${lastName}`);
---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+const contactForm = document.querySelector('.contact-form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+
+contactForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+    const formData = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+
+}) 
